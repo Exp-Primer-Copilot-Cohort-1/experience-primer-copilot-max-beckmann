7c54f44ad86d7f673300bf7988131ed3cad2f353
Maximilian Beckmann <43379987+max-beckmann@users.noreply.github.com>
2023-12-19 22:04:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,39 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberValue = member.options[member.selectedIndex].value;
+    var memberText = member.options[member.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+    var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+
+    if (memberValue == "1") {
+        document.getElementById("memberSkills").style.display = "block";
+        document.getElementById("memberSkills").innerHTML = "<option value='1'>1</option><option value='2'>2</option><option value='3'>3</option>";
+    } else if (memberValue == "2") {
+        document.getElementById("memberSkills").style.display = "block";
+        document.getElementById("memberSkills").innerHTML = "<option value='1'>1</option><option value='2'>2</option>";
+    } else if (memberValue == "3") {
+        document.getElementById("memberSkills").style.display = "block";
+        document.getElementById("memberSkills").innerHTML = "<option value='1'>1</option>";
+    } else {
+        document.getElementById("memberSkills").style.display = "none";
+    }
+}
